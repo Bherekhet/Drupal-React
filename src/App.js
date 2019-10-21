7114f116566c71aa49222b72b1100573adfa5e34
= <=>
2019-10-21 15:01:19 -0400

    API is not fully functionaly so is part of displaying recipes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import './App.css';
 import {LoremIpsum} from "lorem-ipsum";
-import { truncate } from 'fs';
 // lorem.generateWords(1);
 //lorem.generateSentences(5);
 //lorem.generateParagraphs(7);
 
-const list = [
-  {
-    objectID: 0,
-    recipeName: "Doro Wat",
-    description: "Made only for certain occations",
-    url: "https://amboethiopiancuisine.files.wordpress.com/2011/06/013011-035.jpg?w=1800&h=720&crop=1)"
-  },
-  {
-    objectID: 1,
-    recipeName: "Shiro",
-    description: "Daily blessed feast",
-    url: "https://www.wassethiopianrestaurant.com/images/10-promo-slides/Shiro-Wat.jpg",
-  }
-]
-
+const url = "http://gtest.dev.wwbtc.com/json/rec";
 const lorem = new LoremIpsum({
   sentencesPerParagraph: {
     max: 8,
@@ -32,14 +17,33 @@ const lorem = new LoremIpsum({
   }
 })
 
+// const list = [
+//   {
+//     objectID: 0,
+//     recipeName: "Doro Wat",
+//     description: "Made only for certain occations",
+//     url: "https://amboethiopiancuisine.files.wordpress.com/2011/06/013011-035.jpg?w=1800&h=720&crop=1)",
+//     detail: lorem.generateSentences(5),
+//     need: lorem.generateWords(3)
+//   },
+//   {
+//     objectID: 1,
+//     recipeName: "Shiro",
+//     description: "Daily blessed feast",
+//     url: "https://www.wassethiopianrestaurant.com/images/10-promo-slides/Shiro-Wat.jpg",
+//     detail: lorem.generateSentences(5),
+//     need: lorem.generateWords(3)  
+//   }
+// ]
+
+
+
 class App extends React.Component {
-  
   render(){
     return(
       <div className="container">
         <Menu />
-        <Home 
-        />
+        <Home />
         <Footer />
       </div>
     );
@@ -48,11 +52,14 @@ class App extends React.Component {
 function Menu (){
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Recipe Magazine</a>
+      <a className="navbar-brand" href="/home">Recipe Magazine</a>
         <div className="navbar-collapse" id="navbarNav">
           <ul className="nav navbar-nav">
-            <li>
-              <a  href="#">Home</a>
+            <li className="nav-item">
+              <a className="nav-link" href="/home">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="./recipes">Recipes</a>
             </li>
           </ul>
         </div>
@@ -73,38 +80,58 @@ class Home extends React.Component {
     super(props);
 
     this.state = {
-      list,
+      list:null,
       clicked: false,
+      id:'',
+      data: []
     };
     this.onRecipeClick = this.onRecipeClick.bind(this);
+    // this.listOfRecipes = this.listOfRecipes.bind(this);
   }
 
-    onRecipeClick(id){
-      {console.log(id)}
-      this.setState({
-        clicked: true
-      });
-    }
-  
+  onRecipeClick(id){
+    this.setState({
+      clicked: true,
+      id: id,
+    });
+  }
+
+  componentDidMount(){
+    fetch("http://gtest.dev.wwbtc.com/json/rec/").then(response=> {
+      //console.log(response);
+      return response.json();
+    }).then(data => {
+      this.setState({list: data})
+      console.log(data);
+    }).catch(err => {
+      console.log("Error Reading data "+ err);
+    })
+
+  }
+    
   render(){
+
     const {list, clicked} = this.state;
+    if (!list) {
+      return null;
+    }
     if (clicked){
-      return <Recipe recipeID={this.id}/>
+      return <Recipe recipeID={this.state.id} recipeList={list}/>
     } 
     return( 
       <div className="home">
         <div className="mainContent">
           {lorem.generateParagraphs(3)}
         </div>
-          { list.map(item => 
-            <div className="card" key={item.objectID}>
-              
+          { list.map((item, key) => 
+            <div className="card" key={key}>
+              {console.log(item.view_node)}
               <a href="#" onClick={() => this.onRecipeClick(item.objectID)}>
-              <img className="card-img-top" src={item.url} alt={item.recipeName}/>
+              <img className="card-img-top" src={item.field_images} alt={item.title}/>
               </a>
               <div className="card-body">
-                <h5 className="card-title">{item.recipeName}</h5>
-                <p className="card-text">{item.description}</p>
+                <h5 className="card-title">{item.title}</h5>
+                <p className="card-text">{item.body}</p>
               </div>
             </div>
             )}
@@ -114,19 +141,22 @@ class Home extends React.Component {
 }
 
 class Recipe extends React.Component {
-  constructor(props){
-    super(props);
 
-    this.state = {
-      recipeID: ''
-    }
-  }
   render(){
+    console.log(this.props.recipeID);
+    console.log(this.props.recipeList);
     return(
       <div>
-        <div className="recipeDetail">
           <p className="recipeTitle">Doro Wat</p>
-          <img className="rounded mx-auto d-block" style={{width: '50%'}} src="https://amboethiopiancuisine.files.wordpress.com/2011/06/013011-035.jpg?w=1800&h=720&crop=1)"></img>
+          <div className="card" style={{width: '18rem'}}>
+            <img className="rounded mx-auto d-block" style={{width: '80%'}} 
+              src="https://amboethiopiancuisine.files.wordpress.com/2011/06/013011-035.jpg?w=1800&h=720&crop=1)">
+              </img>
+            <div className="card-body">
+              <button style={{width: "100px"}} className="btn btn-primary previous">Previous</button>
+              <button style={{width: "100px"}} className="btn btn-primary next">Next</button>
+            </div>
+          </div>
           <p className="detail">{lorem.generateSentences(8)}</p>
           <p className="recipeTitle">What you will need to make the dish</p>
           <ul>
@@ -136,10 +166,7 @@ class Recipe extends React.Component {
             <li>{lorem.generateSentences(1)}</li>
             <li>{lorem.generateSentences(1)}</li>
           </ul>
-          
-        </div>
       </div>
-      
     )
   }
 }
